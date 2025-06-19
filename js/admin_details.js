0ddc98eb2976ba84772390cc3400d673fc2d89ec
Youssef-Afify <youssef.afify05@eng-st.cu.edu.eg>
2025-06-19 07:55:56 +0300

    collecting codes in the 'Full Project' folder

--- js/admin_details.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("delete").addEventListener("click", function () {
-    document.getElementById("details_menu_1").style.display = "none";
-    document.getElementById("details_menu_2").style.display = "block";
-});
-document.getElementById("cancel").addEventListener("click", function () {
-    document.getElementById("details_menu_1").style.display = "block";
-    document.getElementById("details_menu_2").style.display = "none";
-});
--- php/disconnect.php
+++
@@ -1,8 +0,0 @@
-<!-- Sign Out -->
-
-<?php
-include "db.php";
-session_unset();
-session_destroy();
-header("location: index.php");
-?>
